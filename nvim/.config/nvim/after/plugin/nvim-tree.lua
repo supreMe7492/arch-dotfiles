bf9e3645c2ec4a714b93178788f9c541dd19621d
supreMe7492 <supremeraut94@gmail.com>
2025-11-21 20:41:49 +0545

    some changes

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -2,46 +2,48 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require('nvim-tree').setup {
-  view = {
-    side = 'right',           
-    width = 30,
-  --`  hide_root_folder = false,
-  },
-  actions = {
-    open_file = {
-      quit_on_open = true,  -- closes tree when opening a file
-    },
-  },
-  filters = {
-    dotfiles = false,          
-  },
-  renderer = {
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        git = false,
-      },
-    },
-  },
-  hijack_cursor = true,
-  hijack_netrw = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  }
-}
+require("nvim-tree").setup({
+	view = {
+		side = "right",
+		width = 30,
+		--`  hide_root_folder = false,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true, -- closes tree when opening a file
+		},
+	},
+	git = {
+		ignore = false,
+	},
+	filters = {
+		dotfiles = false,
+	},
+	renderer = {
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				git = false,
+			},
+		},
+	},
+	hijack_cursor = true,
+	hijack_netrw = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+})
 
 -- toggle keymap
-vim.keymap.set('n', '\\', '<Cmd>NvimTreeToggle<CR>', {silent = true})
-
+vim.keymap.set("n", "\\", "<Cmd>NvimTreeToggle<CR>", { silent = true })
